56c380340009a3f2ca901f381c0448592fca72aa
StaicuBogdan <61222625+StaicuBogdan@users.noreply.github.com>
2022-04-29 21:03:40 +0300

    Mutations
    Added mutation testing for the function

--- function_M2.py
+++ function_M2.py
@@ -1,16 +1,17 @@
-# functie care returneaza numarul de studenti care au media peste 5 (media se calculeaza ponderat in functie de nr de credite)
-# sau un mesaj de eroare in functie de validitatea datelor.
-# functia primeste ca parametrii o lista de studenti, una de cursuri si una de note
-# cerinte pentru ca datele sa fie valide
-# 1. toti studentii si toate cursurile din lista de note trebuie sa fie prezente si in listele lor respective
-# 2. toate notele sa fie intre 1 si 10
-# 3. toti studentii sa aiba note la toate materiile
-# 4. toate elementele din lista trebuie sa fie unice
-# 5. toti studentii trebuie sa fie intr-un an intre 1 si 4
-# Pentru ca un student sa fie trecut trbuie ca media lui sa fie peste 5
+"""
+Functia de countPassedStudentsM1 este un mutant al functiei countPassedStudents, un n-order-mutant, n = 3
+avand ca modificari la linia 26, (student.year < 1 or student.year > 4)  -> student.year < 1
+la linia 45, grade.grade < 1  -> grade.grade < 9
+la linia 58,  !=  ->  ==
+Rezultatul este acesta: 9 teste omoara mutantul acesta, dintre care 8 dau FAIL, iar unul Eroare din cauza valorilor
+schimbate in functie ca urmare a mutatiei, facand astfel diferenta intre programul initial si cel mutant.
+
+Programul nu este doar adus pe alta ramura a acestuia, rezultatul returnat este complet diferit din cauza mutatiei,
+rezultanda ca mutatia countPassedStudentsM2 este una puternica (Strong Mutation).
+"""
 
 
-def countPassedStudents(students, courses, grades):
+def countPassedStudentsM2(students, courses, grades):
     studentsDict = {}
     coursesDict = {}
     studentsScore = {}
@@ -22,7 +23,7 @@ def countPassedStudents(students, courses, grades):
         if student.firstName + student.lastName in studentsDict.keys():
             return 'DUPLICATE_STUDENTS'
         else:
-            if student.year < 1 or student.year > 4:
+            if student.year < 1:
                 return 'INVALID_STUDENT_YEAR'
             studentsDict[student.firstName + student.lastName] = student
             studentsScore[student.firstName + student.lastName] = 0
@@ -41,7 +42,7 @@ def countPassedStudents(students, courses, grades):
             return 'GRADE_STUDENT_DOES_NOT_EXIST'
         if grade.course.name not in coursesDict.keys():
             return 'GRADE_COURSE_DOES_NOT_EXIST'
-        if grade.grade < 1 or grade.grade > 10:
+        if grade.grade < 9 or grade.grade > 10:
             return 'INVALID_GRADE_VALUE'
         studentsScore[grade.student.firstName + grade.student.lastName] += grade.grade * grade.course.noOfCredits
         if grade.student.firstName + grade.student.lastName not in gradedCoursesPerStudent.keys():
@@ -54,7 +55,7 @@ def countPassedStudents(students, courses, grades):
                     *gradedCoursesPerStudent[grade.student.firstName + grade.student.lastName], grade.course.name]})
 
     for student in studentsScore.keys():
-        if len(gradedCoursesPerStudent[student]) != len(courses):
+        if len(gradedCoursesPerStudent[student]) == len(courses):
             return 'NOT_ALL_COURSES_HAVE_BEEN_GRADED'
         if studentsScore[student] / totalCredits >= 5:
             noOfPassedStudents += 1
